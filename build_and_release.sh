31d58c2c030a2b15c411a2403464bfdeafd5abf9
vo0doo <kirsanov.bvt@gmail.com>
2022-06-01 02:40:30 +0300

    update build_and_release.sh

--- build_and_release.sh
+++ build_and_release.sh
@@ -3,18 +3,15 @@
 
 login_on_heroku() {
     bash -c "heroku container:login"
-    return
 }
 
 release_on_heroku() {
     bash -c "heroku container:release web"
-    return
 }
 
 
 build_image_on_heroku() {
     bash -c "heroku container:push web"
-    return
 }
 # heroku container:push web && heroku container:release web
 build_image_on_heroku && release_on_heroku
