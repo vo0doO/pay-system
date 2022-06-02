45171871cc41c0b27bd8b8eac9642c44af4711dc
vo0doo <kirsanov.bvt@gmail.com>
2022-06-03 00:03:16 +0300

    feat: create web_money.py

---
+++ psa/utils/web_money.py
@@ -0,0 +1,34 @@
+INIT = {
+    "RUB": 719,
+    }
+ONE = {
+    "RUB/MWZ": 53,
+    "RUB/USDT": 64,
+    } 
+TWO = {
+    "WMZ/RUB": 56.92
+    }
+COMSA = {
+    "RUB/MWZ": {
+        "procents": 0.025,
+        "fix": 40,
+    },
+    "WMZ/RUB": {
+        "procents": 0.025,
+        "fix": 40,
+    },
+}
+
+
+def rub_wmz_rub(INIT, COMSA, ONE, TWO):
+
+    state_ONE = (INIT - (INIT * COMSA["RUB/WMZ"]["procents"]))/ONE
+    
+    state_TWO = ( state_ONE - (( state_ONE * COMSA["MWZ/RUB"]["procents"] ) + COMSA["WMZ/RUB"]["fix"] ) ) * TWO
+    
+    out = state_TWO
+    
+    print(f"\n \n Отдам {INIT} RUB --> Получу {state_ONE} MWZ --> {out} RUB верну \n \n")
+
+
+rub_wmz_rub(INIT=INIT["RUB"],COMSA=COMSA, ONE=ONE["RUB/MWZ"],TWO=TWO["WMZ/RUB"])
